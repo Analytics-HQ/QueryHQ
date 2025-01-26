1be5a40924210bd0887a83441b16b025d06a6c7d
Chad Cravens <ccravens@MacBook-Pro.localdomain>
2025-01-25 20:25:51 -0500

    Implementation of monaco code editor

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EditorComponent} from 'ngx-monaco-editor-v2';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [FormsModule, EditorComponent],
+  templateUrl: 'app.component.html',
+  styleUrl: 'app.component.scss'
 })
-export class AppComponent {
-  title = 'web';
+export class AppComponent implements OnInit {
+  editor: any;
+
+  code: string = '';
+
+  options = {
+    theme: 'vs-light',
+    language: 'sql'
+  };
+
+  ngOnInit() {}
+
+  onInit(editor: any) {
+    // this.editor = editor;
+  }
 }
--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,8 +1,54 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {NgxMonacoEditorConfig, provideMonacoEditor} from 'ngx-monaco-editor-v2';
 import { routes } from './app.routes';
 
+declare var monaco: any;
+
+export function onMonacoLoad() {
+
+  console.log((window as any).monaco);
+
+  const uri = monaco.Uri.parse('a://b/foo.json');
+  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+    validate: true,
+    schemas: [{
+      uri: 'http://myserver/foo-schema.json',
+      fileMatch: [uri.toString()],
+      schema: {
+        type: 'object',
+        properties: {
+          p1: {
+            enum: ['v1', 'v2']
+          },
+          p2: {
+            $ref: 'http://myserver/bar-schema.json'
+          }
+        }
+      }
+    }, {
+      uri: 'http://myserver/bar-schema.json',
+      fileMatch: [uri.toString()],
+      schema: {
+        type: 'object',
+        properties: {
+          q1: {
+            enum: ['x1', 'x2']
+          }
+        }
+      }
+    }]
+  });
+
+}
+
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  baseUrl: 'assets',
+  defaultOptions: { scrollBeyondLastLine: false },
+  onMonacoLoad
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideMonacoEditor(monacoConfig)]
 };
