fd8415ea1ed68a5bdab272f6a85b1df92cfcb373
Chad Cravens <ccravens@MacBook-Pro.localdomain>
2025-01-26 01:25:54 -0500

    Implemented initial layout of query window

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,20 +1,72 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { EditorComponent } from 'ngx-monaco-editor-v2';
 import { AngularSplitModule } from 'angular-split';
+import { EditorComponent } from 'ngx-monaco-editor-v2';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [FormsModule, EditorComponent, AngularSplitModule],
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  private editorInstance: any; // Store Monaco Editor instance
-  private observer: MutationObserver | null = null; // MutationObserver for layout changes
+  private editorInstance: any;
 
-  code: string = '';
+  code: string = `CREATE TABLE dbo.EmployeePhoto
+(
+    EmployeeId INT NOT NULL PRIMARY KEY,
+    Photo VARBINARY(MAX) FILESTREAM NULL,
+    MyRowGuidColumn UNIQUEIDENTIFIER NOT NULL ROWGUIDCOL
+                    UNIQUE DEFAULT NEWID()
+);
+
+GO
+
+/*
+text_of_comment
+/* nested comment */
+*/
+
+-- line comment
+
+CREATE NONCLUSTERED INDEX IX_WorkOrder_ProductID
+    ON Production.WorkOrder(ProductID)
+    WITH (FILLFACTOR = 80,
+        PAD_INDEX = ON,
+        DROP_EXISTING = ON);
+GO
+
+WHILE (SELECT AVG(ListPrice) FROM Production.Product) < $300
+BEGIN
+   UPDATE Production.Product
+      SET ListPrice = ListPrice * 2
+   SELECT MAX(ListPrice) FROM Production.Product
+   IF (SELECT MAX(ListPrice) FROM Production.Product) > $500
+      BREAK
+   ELSE
+      CONTINUE
+END
+PRINT 'Too much for the market to bear';
+
+MERGE INTO Sales.SalesReason AS [Target]
+USING (VALUES ('Recommendation','Other'), ('Review', 'Marketing'), ('Internet', 'Promotion'))
+       AS [Source] ([NewName], NewReasonType)
+ON [Target].[Name] = [Source].[NewName]
+WHEN MATCHED
+THEN UPDATE SET ReasonType = [Source].NewReasonType
+WHEN NOT MATCHED BY TARGET
+THEN INSERT ([Name], ReasonType) VALUES ([NewName], NewReasonType)
+OUTPUT $action INTO @SummaryOfChanges;
+
+SELECT ProductID, OrderQty, SUM(LineTotal) AS Total
+FROM Sales.SalesOrderDetail
+WHERE UnitPrice < $5.00
+GROUP BY ProductID, OrderQty
+ORDER BY ProductID, OrderQty
+OPTION (HASH GROUP, FAST 10);
+`;
+
   options = {
     theme: 'vs-light',
     language: 'sql',
@@ -22,34 +74,15 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {}
 
-  ngAfterViewInit(): void {
-    // Observe changes in the pane layout
-    const editorPane = document.querySelector('.editor-pane');
-    if (editorPane) {
-      this.observer = new MutationObserver(() => {
-        if (this.editorInstance) {
-          this.editorInstance.layout(); // Adjust Monaco Editor layout
-        }
-      });
+  ngAfterViewInit() {}
 
-      this.observer.observe(editorPane, {
-        attributes: true,
-        childList: false,
-        subtree: false,
-      });
-    }
-  }
-
-  // Capture the Monaco Editor instance
   onInit(editor: any): void {
     this.editorInstance = editor;
   }
 
-  // Clean up the MutationObserver when the component is destroyed
-  ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.disconnect();
-      this.observer = null;
+  onGutterResize(): void {
+    if (this.editorInstance) {
+      this.editorInstance.layout();
     }
   }
 }
